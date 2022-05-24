6a41be5f9ead7916cdfddb6dd38b8a556ac8d494
yangshengkai <skyeinfo@qq.com>
2022-05-24 15:16:26 +0800

    Update main.go
    update

--- Practice/两个协程交替输出1-10/p2/main.go
+++ Practice/两个协程交替输出1-10/p2/main.go
@@ -6,32 +6,58 @@ import (
 )
 
 func main() {
-	Solution()
+	Solution1()
 }
 
-func Solution() {
+//func Solution() {
+//	wg := sync.WaitGroup{}
+//
+//	ch := make(chan int)
+//
+//	fmt.Println(ch)
+//
+//	wg.Add(2)
+//	go func() {
+//		i := 1
+//		for ; i <= 10; i += 2 {
+//			fmt.Printf("curr_a: %d\n", i)
+//			ch <- 1
+//			<-ch
+//		}
+//		wg.Done()
+//	}()
+//	go func() {
+//		i := 2
+//		for ; i <= 10; i += 2 {
+//			<-ch
+//			fmt.Printf("curr_b: %d\n", i)
+//			ch <- 1
+//		}
+//		wg.Done()
+//	}()
+//	wg.Wait()
+//
+//	close(ch)
+//}
+
+func Solution1() {
+	var a int
 	wg := sync.WaitGroup{}
 
 	ch := make(chan int)
 
 	fmt.Println(ch)
 
-	wg.Add(2)
+	wg.Add(1)
 	go func() {
 		i := 1
 		for ; i <= 10; i += 2 {
 			fmt.Printf("curr_a: %d\n", i)
 			ch <- 1
-			<-ch
-		}
-		wg.Done()
-	}()
-	go func() {
-		i := 2
-		for ; i <= 10; i += 2 {
-			<-ch
-			fmt.Printf("curr_b: %d\n", i)
-			ch <- 1
+
+			fmt.Println("123213")
+			a = <-ch
+			fmt.Println(a)
 		}
 		wg.Done()
 	}()
